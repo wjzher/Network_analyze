156e9e13fbad2c1c9e4589ddc4e7bf8652e1fdd5
wjzhe <wjzher@gmail.com>
2018-05-29 13:56:51 +0800

    可将tcp包保存到log.txt文件中
    Signed-off-by: wjzhe <wjzher@gmail.com>

--- network_analyze/network_analyze.cpp
+++ network_analyze/network_analyze.cpp
@@ -7,54 +7,58 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define ETHERTYPE_IP 0x0800 /* ip protocol */
-#define TCP_PROTOCAL 0x0600 /* tcp protocol */
-#define ETHER_ADDR_LEN 6
-#define BUFFER_MAX_LENGTH 65536
-/*
-* define struct of ethernet header , ip address , ip header and tcp header
-*/
-/* ethernet header */
-typedef struct ether_header {
-	u_char ether_shost[ETHER_ADDR_LEN]; /* source ethernet address, 8 bytes */
-	u_char ether_dhost[ETHER_ADDR_LEN]; /* destination ethernet addresss, 8 bytes */
-	u_short ether_type;                 /* ethernet type, 16 bytes */
-} ether_header;
+//some packet processing functions
+void ProcessPacket(const u_char*, int); //This will decide how to digest
 
-/* four bytes ip address */
-typedef struct ip_address {
-	u_char byte1;
-	u_char byte2;
-	u_char byte3;
-	u_char byte4;
-}ip_address;
+void print_ethernet_header(const u_char*);
+void PrintIpHeader(const u_char*, int);
+void PrintIcmpPacket(const u_char*, int);
+void print_udp_packet(const u_char*, int);
+void PrintTcpPacket(const u_char*, int);
+void PrintData(const u_char*, int);
 
-/* ipv4 header */
-typedef struct ip_header {
-	u_char ver_ihl;         /* version and ip header length */
-	u_char tos;             /* type of service */
-	u_short tlen;           /* total length */
-	u_short identification; /* identification */
-	u_short flags_fo;       // flags and fragment offset
-	u_char ttl;             /* time to live */
-	u_char proto;           /* protocol */
-	u_short crc;            /* header checksum */
-	struct in_addr saddr;       /* source address */
-	struct in_addr daddr;       /* destination address */
-	u_int op_pad;           /* option and padding */
-}ip_header;
+// Set the packing to a 1 byte boundary
+//#include "pshpack1.h"
+//Ethernet Header
+typedef struct ethernet_header
+{
+	UCHAR dest[6];
+	UCHAR source[6];
+	USHORT type;
+}   ETHER_HDR, *PETHER_HDR, FAR * LPETHER_HDR, ETHERHeader;
 
-/* tcp header */
-//typedef struct tcp_header {
-//	u_short th_sport;         /* source port */
-//	u_short th_dport;         /* destination port */
-//	u_int th_seq;             /* sequence number */
-//	u_int th_ack;             /* acknowledgement number */
-//	u_short th_len_resv_code; /* datagram length and reserved code */
-//	u_short th_window;        /* window */
-//	u_short th_sum;           /* checksum */
-//	u_short th_urp;           /* urgent pointer */
-//}tcp_header;
+//Ip header (v4)
+typedef struct ip_hdr
+{
+	unsigned char ip_header_len : 4; // 4-bit header length (in 32-bit words) normally=5 (Means 20 Bytes may be 24 also)
+	unsigned char ip_version : 4; // 4-bit IPv4 version
+	unsigned char ip_tos; // IP type of service
+	unsigned short ip_total_length; // Total length
+	unsigned short ip_id; // Unique identifier
+
+	unsigned char ip_frag_offset : 5; // Fragment offset field
+
+	unsigned char ip_more_fragment : 1;
+	unsigned char ip_dont_fragment : 1;
+	unsigned char ip_reserved_zero : 1;
+
+	unsigned char ip_frag_offset1; //fragment offset
+
+	unsigned char ip_ttl; // Time to live
+	unsigned char ip_protocol; // Protocol(TCP,UDP etc)
+	unsigned short ip_checksum; // IP checksum
+	unsigned int ip_srcaddr; // Source address
+	unsigned int ip_destaddr; // Source address
+} IPV4_HDR;
+
+//UDP header
+typedef struct udp_hdr
+{
+	unsigned short source_port; // Source port no.
+	unsigned short dest_port; // Dest. port no.
+	unsigned short udp_length; // Udp packet length
+	unsigned short udp_checksum; // Udp checksum (optional)
+} UDP_HDR;
 
 // TCP header
 typedef struct tcp_header
@@ -88,145 +92,394 @@ typedef struct tcp_header
 	unsigned short urgent_pointer; // urgent pointer
 } TCP_HDR;
 
-/* packet handler 函数原型 */
-/* 每次捕获到数据包时，libpcap都会自动调用这个回调函数 */
+typedef struct icmp_hdr
+{
+	BYTE type; // ICMP Error type
+	BYTE code; // Type sub code
+	USHORT checksum;
+	USHORT id;
+	USHORT seq;
+} ICMP_HDR;
+// Restore the byte boundary back to the previous value
+//#include <poppack.h>
+
+FILE *logfile;
+int tcp = 0, udp = 0, icmp = 0, others = 0, igmp = 0, total = 0, i, j;
+struct sockaddr_in source, dest;
+char hex[2];
+
+//Its free!
+ETHER_HDR *ethhdr;
+IPV4_HDR *iphdr;
+TCP_HDR *tcpheader;
+UDP_HDR *udpheader;
+ICMP_HDR *icmpheader;
+const u_char *data;
+
 void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data)
 {
 	struct tm *ltime;
-	char timestr[50];
+	char timestr[80];
 	time_t local_tv_sec;
-
-	ether_header * eheader = (ether_header*)pkt_data; /* transform packet data to ethernet header */
-	if (eheader->ether_type != htons(ETHERTYPE_IP)) { /* ip packet only */
-		return;
-	}
-	ip_header * ih = (ip_header*)(pkt_data + 14); /* get ip header */
-	if (ih->proto != htons(TCP_PROTOCAL)) { /* tcp packet only */
-		return;
-	}
 
 	/* 将时间戳转换成可识别的格式 */
 	local_tv_sec = header->ts.tv_sec;
 	ltime = localtime(&local_tv_sec);
 	strftime(timestr, sizeof timestr, "%Y-%m-%d %H:%M:%S", ltime);
-
-	printf("%s,%.3d len:%d\n", timestr, header->ts.tv_usec / 1000, header->len);
-
-	int ip_len = ntohs(ih->tlen); /* get ip length, it contains header and body */
-
-	char* ip_pkt_data = (char*)ih;
-	char buffer[BUFFER_MAX_LENGTH];
-	int bufsize = 0;
-
-	printf("---------IP协议---------\n");
-	printf("版本号:%d\n", ih->ver_ihl);
-	printf("总长度:%d\n", ntohs(ih->tlen));
-	printf("协议类型:%d\n", ih->proto);
-	printf("检验和:%d\n", ntohs(ih->crc));
-	printf("源IP地址:%s\n", inet_ntoa(ih->saddr));
-	printf("目的地址:%s\n", inet_ntoa(ih->daddr));
-	ip_len = (ih->ver_ihl & 0xf) * 4;
-	tcp_header *th = (tcp_header *)((u_char*)ih + ip_len);
-	u_short sport, dport;
-	/* 将网络字节序列转换成主机字节序列 */
-	sport = ntohs(th->th_sport);
-	dport = ntohs(th->th_dport);
-	printf("tcp: src port %d dst port %d\n", sport, dport);
+	//print pkt timestamp and pkt len
+	//fprintf(logfile , "\nNext Packet : %ld:%ld (Packet Length : %ld bytes) " , header->ts.tv_sec, header->ts.tv_usec, header->len);
+	fprintf(logfile, "\nNext Packet : %s.%ld (Packet Length : %ld bytes) ", timestr, header->ts.tv_usec / 1000, header->len);
+	ProcessPacket(pkt_data, header->caplen);
 	return;
 }
 
 int main()
 {
-	pcap_if_t *alldevs;
-	pcap_if_t *d;
-	int inum;
-	int i = 0;
-	pcap_t *adhandle;
+	u_int i, res, inum;
 	char errbuf[PCAP_ERRBUF_SIZE];
-	struct bpf_program fcode;
-	char packet_filter[] = "ip and tcp";
-	u_int netmask;
+	char buffer[100];
+	u_char *pkt_data;
+	time_t seconds;
+	struct tm tbreak;
+	pcap_if_t *alldevs, *d;
+	pcap_t *fp;
+	struct pcap_pkthdr *header;
 
-	/* 获取本机设备列表 */
-	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL, &alldevs, errbuf) == -1)
+	fopen_s(&logfile, "log.txt", "w");
+
+	if (logfile == NULL)
 	{
-		fprintf(stderr, "Error in pcap_findalldevs: %s\n", errbuf);
-		exit(1);
+		printf("Unable to create file.");
 	}
 
-	/* 打印列表 */
+	/* The user didn't provide a packet source: Retrieve the local device list */
+	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL, &alldevs, errbuf) == -1)
+	{
+		fprintf(stderr, "Error in pcap_findalldevs_ex: %s\n", errbuf);
+		return -1;
+	}
+
+	i = 0;
+	/* Print the list */
 	for (d = alldevs; d; d = d->next)
 	{
-		printf("%d. %s", ++i, d->name);
+		printf("%d. %s\n    ", ++i, d->name);
+
 		if (d->description)
+		{
 			printf(" (%s)\n", d->description);
+		}
 		else
+		{
 			printf(" (No description available)\n");
+		}
 	}
 
 	if (i == 0)
 	{
-		printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
+		fprintf(stderr, "No interfaces found! Exiting.\n");
 		return -1;
 	}
 
-	printf("Enter the interface number (1-%d):", i);
-	scanf("%d", &inum);
+	printf("Enter the interface number you would like to sniff : ");
+	scanf_s("%d", &inum);
 
-	if (inum < 1 || inum > i)
+	/* Jump to the selected adapter */
+	for (d = alldevs, i = 0; i < inum - 1; d = d->next, i++);
+
+	/* Open the device */
+	if ((fp = pcap_open(d->name,
+		65536 /*snaplen*/,
+		PCAP_OPENFLAG_PROMISCUOUS /*flags*/,
+		1000 /*read timeout*/,
+		NULL /* remote authentication */,
+		errbuf)
+		) == NULL)
 	{
-		printf("\nInterface number out of range.\n");
+		fprintf(stderr, "\nError opening adapter\n");
 		/* 释放设备列表 */
 		pcap_freealldevs(alldevs);
 		return -1;
 	}
-
-	/* 跳转到选中的适配器 */
-	for (d = alldevs, i = 0; i< inum - 1; d = d->next, i++);
-
-	/* 打开设备 */
-	if ((adhandle = pcap_open(d->name,          // 设备名
-		65536,            // 65535保证能捕获到不同数据链路层上的每个数据包的全部内容
-		PCAP_OPENFLAG_PROMISCUOUS,    // 混杂模式
-		1000,             // 读取超时时间
-		NULL,             // 远程机器验证
-		errbuf            // 错误缓冲池
-	)) == NULL)
-	{
-		fprintf(stderr, "\nUnable to open the adapter. %s is not supported by WinPcap\n", d->name);
-		/* 释放设备列表 */
-		pcap_freealldevs(alldevs);
-		return -1;
-	}
-
-	if (d->addresses != NULL) {
-		// 获得接口的第一个地址的掩码  
-		netmask = ((struct sockaddr_in*)(d->addresses->netmask))->sin_addr.S_un.S_addr;
-	} else {
-		netmask = 0xffffff;
-	}
-	//// 编译过滤器  
-	//if (pcap_compile(adhandle, &fcode, packet_filter, 1, netmask) < 0) {
-	//	fprintf(stderr, "\nUnable to compile the packet filter.Check the syntax\n");
-	//	pcap_freealldevs(alldevs);
-	//	return -1;
-	//}
-
-	//// 设置过滤器  
-	//if (pcap_setfilter(adhandle, &fcode) < 0) {
-	//	fprintf(stderr, "\nError setting the filter.\n");
-	//	pcap_freealldevs(alldevs);
-	//	return -1;
-	//}
-
-	printf("\nlistening on %s...\n", d->description);
 
 	/* 释放设备列表 */
 	pcap_freealldevs(alldevs);
-
-	/* 开始捕获 */
-	pcap_loop(adhandle, 0, packet_handler, NULL);
+	/* 开始捕捉 */
+	pcap_loop(fp, 0, packet_handler, NULL);
 
 	return 0;
 }
 
+void ProcessPacket(const u_char* Buffer, int Size)
+{
+	//Ethernet header
+	ethhdr = (ETHER_HDR *)Buffer;
+	++total;
+
+	//Ip packets
+	if (ntohs(ethhdr->type) == 0x0800)
+	{
+		//ip header
+		iphdr = (IPV4_HDR *)(Buffer + sizeof(ETHER_HDR));
+
+		switch (iphdr->ip_protocol) //Check the Protocol and do accordingly...
+		{
+		case 1: //ICMP Protocol
+			icmp++;
+			//PrintIcmpPacket(Buffer, Size);
+			break;
+
+		case 2: //IGMP Protocol
+			igmp++;
+			break;
+
+		case 6: //TCP Protocol
+			tcp++;
+			PrintTcpPacket(Buffer, Size);
+			break;
+
+		case 17: //UDP Protocol
+			udp++;
+			//print_udp_packet(Buffer, Size);
+			break;
+
+		default: //Some Other Protocol like ARP etc.
+			others++;
+			break;
+		}
+	}
+
+	printf("TCP : %d UDP : %d ICMP : %d IGMP : %d Others : %d Total : %d\r", tcp, udp, icmp, igmp, others, total);
+}
+
+/*
+Print the Ethernet header
+*/
+void print_ethernet_header(const u_char* buffer)
+{
+	ETHER_HDR *eth = (ETHER_HDR *)buffer;
+
+	fprintf(logfile, "\n");
+	fprintf(logfile, "Ethernet Header\n");
+	fprintf(logfile, " |-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->dest[0], eth->dest[1], eth->dest[2], eth->dest[3], eth->dest[4], eth->dest[5]);
+	fprintf(logfile, " |-Source Address      : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->source[0], eth->source[1], eth->source[2], eth->source[3], eth->source[4], eth->source[5]);
+	fprintf(logfile, " |-Protocol            : 0x%.4x \n", ntohs(eth->type));
+}
+
+/*
+Print the IP header for IP packets
+*/
+void PrintIpHeader(const unsigned char* Buffer, int Size)
+{
+	int iphdrlen = 0;
+
+	iphdr = (IPV4_HDR *)(Buffer + sizeof(ETHER_HDR));
+	iphdrlen = iphdr->ip_header_len * 4;
+
+	memset(&source, 0, sizeof(source));
+	source.sin_addr.s_addr = iphdr->ip_srcaddr;
+
+	memset(&dest, 0, sizeof(dest));
+	dest.sin_addr.s_addr = iphdr->ip_destaddr;
+
+	print_ethernet_header(Buffer);
+
+	fprintf(logfile, "\n");
+	fprintf(logfile, "IP Header\n");
+	fprintf(logfile, " |-IP Version : %d\n", (unsigned int)iphdr->ip_version);
+	fprintf(logfile, " |-IP Header Length : %d DWORDS or %d Bytes\n", (unsigned int)iphdr->ip_header_len, ((unsigned int)(iphdr->ip_header_len)) * 4);
+	fprintf(logfile, " |-Type Of Service : %d\n", (unsigned int)iphdr->ip_tos);
+	fprintf(logfile, " |-IP Total Length : %d Bytes(Size of Packet)\n", ntohs(iphdr->ip_total_length));
+	fprintf(logfile, " |-Identification : %d\n", ntohs(iphdr->ip_id));
+	fprintf(logfile, " |-Reserved ZERO Field : %d\n", (unsigned int)iphdr->ip_reserved_zero);
+	fprintf(logfile, " |-Dont Fragment Field : %d\n", (unsigned int)iphdr->ip_dont_fragment);
+	fprintf(logfile, " |-More Fragment Field : %d\n", (unsigned int)iphdr->ip_more_fragment);
+	fprintf(logfile, " |-TTL : %d\n", (unsigned int)iphdr->ip_ttl);
+	fprintf(logfile, " |-Protocol : %d\n", (unsigned int)iphdr->ip_protocol);
+	fprintf(logfile, " |-Checksum : %d\n", ntohs(iphdr->ip_checksum));
+	fprintf(logfile, " |-Source IP : %s\n", inet_ntoa(source.sin_addr));
+	fprintf(logfile, " |-Destination IP : %s\n", inet_ntoa(dest.sin_addr));
+}
+
+/*
+Print the TCP header for TCP packets
+*/
+void PrintTcpPacket(const u_char* Buffer, int Size)
+{
+	unsigned short iphdrlen;
+	int header_size = 0, tcphdrlen, data_size;
+
+	iphdr = (IPV4_HDR *)(Buffer + sizeof(ETHER_HDR));
+	iphdrlen = iphdr->ip_header_len * 4;
+
+	tcpheader = (TCP_HDR*)(Buffer + iphdrlen + sizeof(ETHER_HDR));
+	tcphdrlen = tcpheader->data_offset * 4;
+
+	data = (Buffer + sizeof(ETHER_HDR) + iphdrlen + tcphdrlen);
+	data_size = (Size - sizeof(ETHER_HDR) - iphdrlen - tcphdrlen);
+
+	fprintf(logfile, "\n\n***********************TCP Packet*************************\n");
+
+	PrintIpHeader(Buffer, Size);
+
+	fprintf(logfile, "\n");
+	fprintf(logfile, "TCP Header\n");
+	fprintf(logfile, " |-Source Port : %u\n", ntohs(tcpheader->source_port));
+	fprintf(logfile, " |-Destination Port : %u\n", ntohs(tcpheader->dest_port));
+	fprintf(logfile, " |-Sequence Number : %u\n", ntohl(tcpheader->sequence));
+	fprintf(logfile, " |-Acknowledge Number : %u\n", ntohl(tcpheader->acknowledge));
+	fprintf(logfile, " |-Header Length : %d DWORDS or %d BYTES\n", (unsigned int)tcpheader->data_offset, (unsigned int)tcpheader->data_offset * 4);
+	fprintf(logfile, " |-CWR Flag : %d\n", (unsigned int)tcpheader->cwr);
+	fprintf(logfile, " |-ECN Flag : %d\n", (unsigned int)tcpheader->ecn);
+	fprintf(logfile, " |-Urgent Flag : %d\n", (unsigned int)tcpheader->urg);
+	fprintf(logfile, " |-Acknowledgement Flag : %d\n", (unsigned int)tcpheader->ack);
+	fprintf(logfile, " |-Push Flag : %d\n", (unsigned int)tcpheader->psh);
+	fprintf(logfile, " |-Reset Flag : %d\n", (unsigned int)tcpheader->rst);
+	fprintf(logfile, " |-Synchronise Flag : %d\n", (unsigned int)tcpheader->syn);
+	fprintf(logfile, " |-Finish Flag : %d\n", (unsigned int)tcpheader->fin);
+	fprintf(logfile, " |-Window : %d\n", ntohs(tcpheader->window));
+	fprintf(logfile, " |-Checksum : %d\n", ntohs(tcpheader->checksum));
+	fprintf(logfile, " |-Urgent Pointer : %d\n", tcpheader->urgent_pointer);
+	fprintf(logfile, "\n");
+	fprintf(logfile, " DATA Dump ");
+	fprintf(logfile, "\n");
+
+	fprintf(logfile, "IP Header\n");
+	PrintData((u_char*)iphdr, iphdrlen);
+
+	fprintf(logfile, "TCP Header\n");
+	PrintData((u_char*)tcpheader, tcphdrlen);
+
+	fprintf(logfile, "Data Payload\n");
+	PrintData(data, data_size);
+
+	fprintf(logfile, "\n###########################################################\n");
+}
+
+/*
+Print the UDP header for UDP packets
+*/
+void print_udp_packet(const u_char *Buffer, int Size)
+{
+	int iphdrlen = 0, data_size = 0;
+
+	iphdr = (IPV4_HDR *)(Buffer + sizeof(ETHER_HDR));
+	iphdrlen = iphdr->ip_header_len * 4;
+
+	udpheader = (UDP_HDR*)(Buffer + iphdrlen + sizeof(ETHER_HDR));
+
+	data = (Buffer + sizeof(ETHER_HDR) + iphdrlen + sizeof(UDP_HDR));
+	data_size = (Size - sizeof(ETHER_HDR) - iphdrlen - sizeof(UDP_HDR));
+
+	fprintf(logfile, "\n\n***********************UDP Packet*************************\n");
+
+	PrintIpHeader(Buffer, Size);
+
+	fprintf(logfile, "\nUDP Header\n");
+	fprintf(logfile, " |-Source Port : %d\n", ntohs(udpheader->source_port));
+	fprintf(logfile, " |-Destination Port : %d\n", ntohs(udpheader->dest_port));
+	fprintf(logfile, " |-UDP Length : %d\n", ntohs(udpheader->udp_length));
+	fprintf(logfile, " |-UDP Checksum : %d\n", ntohs(udpheader->udp_checksum));
+
+	fprintf(logfile, "\n");
+
+	fprintf(logfile, "IP Header\n");
+	PrintData((u_char*)iphdr, iphdrlen);
+
+	fprintf(logfile, "UDP Header\n");
+	PrintData((u_char*)udpheader, sizeof(UDP_HDR));
+
+	fprintf(logfile, "Data Payload\n");
+	PrintData(data, data_size);
+
+	fprintf(logfile, "\n###########################################################\n");
+}
+
+void PrintIcmpPacket(const u_char* Buffer, int Size)
+{
+	int iphdrlen = 0, icmphdrlen = 0, data_size = 0;
+
+	iphdr = (IPV4_HDR *)(Buffer + sizeof(ETHER_HDR));
+	iphdrlen = iphdr->ip_header_len * 4;
+
+	icmpheader = (ICMP_HDR*)(Buffer + iphdrlen + sizeof(ETHER_HDR));
+
+	data = (Buffer + sizeof(ETHER_HDR) + iphdrlen + sizeof(ICMP_HDR));
+	data_size = (Size - sizeof(ETHER_HDR) - iphdrlen - sizeof(ICMP_HDR));
+
+	fprintf(logfile, "\n\n***********************ICMP Packet*************************\n");
+	PrintIpHeader(Buffer, Size);
+
+	fprintf(logfile, "\n");
+
+	fprintf(logfile, "ICMP Header\n");
+	fprintf(logfile, " |-Type : %d", (unsigned int)(icmpheader->type));
+
+	if ((unsigned int)(icmpheader->type) == 11)
+	{
+		fprintf(logfile, " (TTL Expired)\n");
+	}
+	else if ((unsigned int)(icmpheader->type) == 0)
+	{
+		fprintf(logfile, " (ICMP Echo Reply)\n");
+	}
+
+	fprintf(logfile, " |-Code : %d\n", (unsigned int)(icmpheader->code));
+	fprintf(logfile, " |-Checksum : %d\n", ntohs(icmpheader->checksum));
+	fprintf(logfile, " |-ID : %d\n", ntohs(icmpheader->id));
+	fprintf(logfile, " |-Sequence : %d\n", ntohs(icmpheader->seq));
+	fprintf(logfile, "\n");
+
+	fprintf(logfile, "IP Header\n");
+	PrintData((u_char*)iphdr, iphdrlen);
+
+	fprintf(logfile, "ICMP Header\n");
+	PrintData((u_char*)icmpheader, sizeof(ICMP_HDR));
+
+	fprintf(logfile, "Data Payload\n");
+	PrintData(data, data_size);
+
+	fprintf(logfile, "\n###########################################################\n");
+}
+
+/*
+Print the hex values of the data
+*/
+void PrintData(const u_char* data, int Size)
+{
+	unsigned char a, line[17], c;
+	int j;
+
+	//loop over each character and print
+	for (i = 0; i < Size; i++)
+	{
+		c = data[i];
+
+		//Print the hex value for every character , with a space
+		fprintf(logfile, " %.2x", (unsigned int)c);
+
+		//Add the character to data line
+		a = (c >= 32 && c <= 128) ? (unsigned char)c : '.';
+
+		line[i % 16] = a;
+
+		//if last character of a line , then print the line - 16 characters in 1 line
+		if ((i != 0 && (i + 1) % 16 == 0) || i == Size - 1)
+		{
+			line[i % 16 + 1] = '\0';
+
+			//print a big gap of 10 characters between hex and characters
+			fprintf(logfile, "          ");
+
+			//Print additional spaces for last lines which might be less than 16 characters in length
+			for (j = strlen((char *)line); j < 16; j++)
+			{
+				fprintf(logfile, "   ");
+			}
+
+			fprintf(logfile, "%s \n", line);
+		}
+	}
+
+	fprintf(logfile, "\n");
+}
